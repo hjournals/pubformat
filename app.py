c65ed2369f2bb07749645978fb90722e68135dba
hjournals <journals@holistence.com>
2026-04-02 09:27:54 +0300

    Update app.py

--- app.py
+++ app.py
@@ -33,4 +33,5 @@ def upload():
     return render_template_string(HTML_FORM)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
